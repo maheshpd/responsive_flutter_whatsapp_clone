18cf48f7a07fb9dd3e675296ca644820edf2d3bf
MaheshI2T <mahesh.prasad@ideas2tech.com>
2022-07-27 12:03:06 +0530

    Done

--- lib/screen/mobile_screen_layout.dart
+++ lib/screen/mobile_screen_layout.dart
@@ -8,60 +8,59 @@ class MobileScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: appBarColor,
-            title: const Text(
-              'Whatsapp',
-              style: TextStyle(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          title: const Text(
+            'Whatsapp',
+            style: TextStyle(
+                color: Colors.grey, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
                   color: Colors.grey,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            centerTitle: false,
-            actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  )),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.more_vert,
-                    color: Colors.grey,
-                  )),
-            ],
-            bottom: const TabBar(
-                indicatorColor: tabColor,
-                indicatorWeight: 4,
-                labelColor: tabColor,
-                unselectedLabelColor: Colors.grey,
-                labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                tabs: [
-                  Tab(
-                    text: 'CHATS',
-                  ),
-                  Tab(
-                    text: 'STATUS',
-                  ),
-                  Tab(
-                    text: 'CALLS',
-                  ),
-                ]),
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: Colors.grey,
+                )),
+          ],
+          bottom: const TabBar(
+              indicatorColor: tabColor,
+              indicatorWeight: 4,
+              labelColor: tabColor,
+              unselectedLabelColor: Colors.grey,
+              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              tabs: [
+                Tab(
+                  text: 'CHATS',
+                ),
+                Tab(
+                  text: 'STATUS',
+                ),
+                Tab(
+                  text: 'CALLS',
+                ),
+              ]),
+        ),
+        body: ContactsList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: tabColor,
+          child: const Icon(
+            Icons.comment,
+            color: Colors.white,
           ),
-          body: ContactsList(),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: tabColor,
-            child: const Icon(
-              Icons.comment,
-              color: Colors.white,
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
